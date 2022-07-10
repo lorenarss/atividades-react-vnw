4b94586c49a6f7b0c33a4cc2e7fd0a1a46c08db1
Lorena Silva <lorena.silva@codeminer42.com>
2022-07-10 12:18:58 -0300

    Fist commit task 01 react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+// Crie uma class component 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+// Guarde em um state as seguintes propriedades: nome, idade, comida favorita, e uma array com pelo menos 3 músicas.
+
+// Renderize na tela:
+// Seu nome em um h1
+// Sua idade em um h2
+// Sua comida favorita em um h3
+// Suas músicas favoritas em uma lista (ul / ol)
+
+class HelloWorld extends Component {
+  state = {
+    name: "Lorena",
+    age: 28,
+    favoriteFood: "açai",
+    musics: ['Helena', "Famous last words", "the ghost of you"]
+
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.name}</h1>
+        <h2>{this.state.age}</h2>
+        <h3>{this.state.favoriteFood}</h3>
+        <ul>
+          <li>{this.state.musics[0]}</li>
+          <li>{this.state.musics[1]}</li>
+          <li>{this.state.musics[2]}</li>
+        </ul>
+        
+      </div>
+    );
+  }
 }
 
-export default App;
+export default HelloWorld;
